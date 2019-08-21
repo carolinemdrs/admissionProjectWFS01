027a5106cf914589994f8b07e15a77634770741b
Caroline Lopes <caroline.lopesm@hotmail.com>
2019-08-21 18:04:07 -0300

    atualização automática da página corrigida

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ function validarEmail () {
 		var formulario = eval("document.forms[0].email");
 		var txt = formulario.value;
 		var bemV = document.getElementById('emails').value;
-
+		
 		if (txt == "" || txt.indexOf("@") == -1 || txt.indexOf(".") == -1) {
 
 		alert  ("Email inválido"); }
 
 		else {
-			alert  (" Bem vindo(a) " + bemV + " !");
+			alert  (" O Email " + bemV + " foi enviado!");
+			
 		}
-		
+			
+document.getElementById("turma").innerHTML = "Bem vindo " + bemV + "!";
 
-		
 	}
